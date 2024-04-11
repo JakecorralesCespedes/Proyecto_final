1b5077018cc4c09c35abd5f7ab276010ff5d26b3
jake <jake.corrales@unadeca.net>
2024-04-10 19:05:39 -0600

    trabajo final

--- app/src/main/java/com/unadeca/proyectofinal/DeliveryProcess.java
+++ app/src/main/java/com/unadeca/proyectofinal/DeliveryProcess.java
@@ -1,6 +1,10 @@
 package com.unadeca.proyectofinal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +19,16 @@ public class DeliveryProcess extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_delivery_process);
+        Button btncerrardelivery = findViewById(R.id.cerrardelivery);
+
+        btncerrardelivery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DeliveryProcess.this, Pago_final.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
--- app/src/main/java/com/unadeca/proyectofinal/Pago_final.java
+++ app/src/main/java/com/unadeca/proyectofinal/Pago_final.java
@@ -1,14 +1,14 @@
-package com.unadeca.proyectofinal;
+        package com.unadeca.proyectofinal;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ClipData;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+        import android.content.ClipData;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.ImageView;
+        import android.widget.TextView;
 
 public class Pago_final extends AppCompatActivity {
 
@@ -18,11 +18,6 @@ public class Pago_final extends AppCompatActivity {
         setContentView(R.layout.activity_pago_final);
         Button btnFlechadevolverse = findViewById(R.id.flechadevolverse);
         ImageView imgSiguiente = findViewById(R.id.siguiente);
-        TextView txtSubtotal =  findViewById(R.id.usd146);
-        TextView txtTarifas =  findViewById(R.id.usd0);
-        TextView txtDelivery =  findViewById(R.id.usd380);
-        TextView txtTotal =  findViewById(R.id.usd14980);
-
 
         btnFlechadevolverse.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,8 +32,12 @@ public class Pago_final extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(Pago_final.this, DeliveryProcess.class);
                 startActivity(intent);
+
+
             }
+
         });
+
 
     }
 
